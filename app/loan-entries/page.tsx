a27cd702d69d2cae69b30d34be1b5d031666b178
Sameea Saqee <sameea.saqee@gmail.com>
2025-04-24 21:43:43 +0500

    added loan entry page

--- app/loan-entries/page.tsx
+++ app/loan-entries/page.tsx
@@ -1,5 +1,22 @@
-export default function LoanEntries(){
-    return (
-        <h1>Hello world</h1>
-    )
+
+export default function LoanEntries() {
+  return (
+    <div>
+      <p>
+        <strong>Title:</strong> Loan for air ticket
+      </p>
+      <p>
+        <strong>Type:</strong> Loan
+      </p>
+      <p>
+        <strong>Other Person: </strong> ABC
+      </p>
+      <p>
+        <strong>Amount: </strong> $500
+      </p>
+      <p>
+      <strong>Date: </strong> 24-04-2025
+      </p>
+    </div>
+  );
 }
