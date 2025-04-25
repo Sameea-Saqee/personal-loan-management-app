3d9507a34378763dacc73ea35b7b158cd25de628
Sameea Saqee <sameea.saqee@gmail.com>
2025-04-25 16:30:53 +0500

    updated

--- app/loan-entries/page.tsx
+++ app/loan-entries/page.tsx
@@ -1,7 +1,8 @@
 
 export default function LoanEntries() {
   return (
-    <div>
+    <div className="flex flex col-3">
+      <h1>My Loan Records</h1>
       <p>
         <strong>Title:</strong> Loan for air ticket
       </p>
@@ -16,6 +17,36 @@ export default function LoanEntries() {
       </p>
       <p>
       <strong>Date: </strong> 24-04-2025
+      </p>
+      <p>
+        <strong>Title:</strong> Loan for laptop
+      </p>
+      <p>
+        <strong>Type:</strong> Loan
+      </p>
+      <p>
+        <strong>Other Person: </strong> XYZ
+      </p>
+      <p>
+        <strong>Amount: </strong> $600
+      </p>
+      <p>
+      <strong>Date: </strong> 14-02-2024
+      </p>
+      <p>
+        <strong>Title:</strong> Loan for rent payment
+      </p>
+      <p>
+        <strong>Type:</strong> Loan
+      </p>
+      <p>
+        <strong>Other Person: </strong> LMN
+      </p>
+      <p>
+        <strong>Amount: </strong> $700
+      </p>
+      <p>
+      <strong>Date: </strong> 10-04-2023
       </p>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-between items-center h-12 mt-32 mx-35">
       <h1>This is my Home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cum ipsa repellendus magni, officia fugit nulla officiis ex qui nihil minus ea sint laboriosam quibusdam voluptates delectus quod! Ad, magnam!</p>
       <Link href="/todolist">
